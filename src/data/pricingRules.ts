9ef043df4b17ea4cc3f5875ee3ab907962eeb761
jay42719 <jariwalajay@outlook.com>
2025-01-03 15:48:35 +0530

    [JJ-03-01-2025] Comment add

--- src/data/pricingRules.ts
+++ src/data/pricingRules.ts
@@ -2,12 +2,12 @@ import { PricingRule } from '../models/pricingRule';
 
 export const pricingRules: PricingRule[] = [
   {
-    sku: 'atv', // Stock Keeping Unit
+    sku: 'atv', // Apple TV
     calculatePrice: (quantity, unitPrice) =>
       Math.floor(quantity / 3) * 2 * unitPrice + (quantity % 3) * unitPrice,
   },
   {
-    sku: 'ipd',
+    sku: 'ipd', // Super iPad
     calculatePrice: (quantity, unitPrice) =>
       quantity > 4 ? quantity * 499.99 : quantity * unitPrice,
   },
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,5 @@
 export type Product = {
-  sku: string;
+  sku: string; // Stock Keeping Unit
   name: string;
   price: number;
 };
